afa04b23046c95f4815c64148a93adfb9dbfdbbb
TCarvalhooo <tamires.carvalho08@aluno.ifce.edu.br>
2025-11-27 17:16:27 -0300

    Delete Avaliações/Avaliação_7/07 directory

--- Avaliações/Avaliação_7/07/github_client/lib/github_oauth_credentials.dart
+++
@@ -1,6 +0,0 @@
-// TODO(CodelabUser): Create an OAuth App
-const githubClientId = 'Ov23liSNHNwW5P3nFXHP';
-const githubClientSecret = '0615aa1416d79ec906aeaef8cb9f192f79294363';
-
-// OAuth scopes for repository and user information
-const githubScopes = ['repo', 'read:org'];
